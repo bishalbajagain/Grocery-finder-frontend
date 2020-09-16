3155a39e60a37bc7c593d3a6dbf1f0965795ac9a
Aquaplus <xy78@hotmail.co.jp>
2020-09-17 07:37:57 +0900

    add function rendering reciepe title and image

--- grocery-finder-frontend/src/RenderRanking.js
+++ grocery-finder-frontend/src/RenderRanking.js
@@ -11,12 +11,60 @@ import { Link } from 'react-router-dom';
 
 
 class RenderRanking extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            arr_json: [{}],
+            category_id: 111
+        };
+    }
 
-    render(){
-        return(
-            <div>{window.location.href}</div>
-        )
+    renderTable() {
+        const arr_json = this.state.arr_json;
+        return (arr_json.map((data, index) => {
+            return (
+                <div>
+                    <Link to={"/recipe-ranking/" + data.categoryId}>
+                        <img src={data.foodImageUrl}  width="128" height="128"/>
+                        <div> recipe title: {data.recipeTitle} </div>
+                    </Link>
+                </div>
+            )
+        }
+        ))
+    };
+
+    render() {
+        if (!this.state.loading) {
+            this.setState({
+                category_id: window.location.href.split('/')[4],
+                loading: true
+            });
+            fetch("http://localhost:8000/api/recipie-catagory-ranking", {
+                method: "get",
+                mode: 'cors',
+                headers: {
+                    "Accept": "application/json",
+                    // "Content-Type": "application/json"
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    this.setState({
+                        loading: true,
+                        arr_json: data
+                    })
+                })
+                .catch(err => {
+                    return (<div> something error  </div>);
+                });
+
+        }
+        return (
+            this.renderTable()
+        );
+
     }
 }
-
 export default withRouter(RenderRanking);
