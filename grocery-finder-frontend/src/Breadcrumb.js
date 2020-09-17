dd18cd6c6f0d0ecc208a0aa7a8753d208d342279
Kazurista <ny77friends@gmail.com>
2020-09-17 14:41:15 +0900

    add logo

---
+++ grocery-finder-frontend/src/Breadcrumb.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Breadcrumb extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            links: [
+                
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <div className="breadcrumb">
+
+            </div>
+        );
+    }
+}
+
+export default Breadcrumb;
